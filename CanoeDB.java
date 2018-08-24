39622d965c0735fd47f1d0790de2811470d42ece
gabrielwilson3 <40298715+gabrielwilson3@users.noreply.github.com>
2018-08-24 11:10:26 -0500

    Add files via upload

---
+++ CanoeDB.java
@@ -0,0 +1,24 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import canoedb.*;
+
+public class CanoeDB {
+
+	public static void main(String[] args)
+	{
+		Database d = new Database("C:\\Users\\1404844039C\\Documents\\JavaProjects\\TestDB_Wagons");
+
+		Query q = new Query( d );
+		
+		q
+			.filter( "wagon_instances", "ERA", "AD" )
+			.output( "people", "EYE_COLOR" )
+			.output( "wagon_types", "FRONT_WHEEL_DIAM" )
+			.execute();
+			
+		System.out.println( "CanoeDB: " + q.map().toString() );
+	}
+	
+}
